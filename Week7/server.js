67773550178ae6826bfdfa02d2e390c9516a3941
sean-rahman-225437344 <s225437344@deakin.edu.au>
2025-09-03 16:57:57 +1000

    My first commit for week7.

---
+++ Week7/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+const http = require('http').createServer(app); // Create HTTP server from app
+const io = require('socket.io')(http);          // Pass http server to socket.io
+
+var port = 3000;
+app.use(express.static('public'));
+let onlineCount = 0;
+
+// Socket connection logic
+io.on('connection', (socket) => {
+  onlineCount++;
+  var date=new Date();
+  console.log('A user connected at '+date.toLocaleTimeString());
+  console.log('Number of users online:'+onlineCount);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected at '+date.toLocaleTimeString());
+    onlineCount--;
+    console.log('Number of users online:'+onlineCount);
+  });
+
+  // Emit a random number every second
+  setInterval(() => {
+    const randomNum = Math.floor(Math.random() * 100);
+    socket.emit('number', randomNum);
+  }, 1000);
+});
+
+// Start server
+http.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
